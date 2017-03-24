21a48be83115a496f0937934c5438f8f764d40b7
zhenxing <zhenxing@qh-speed.com>
2017-03-24 16:23:24 +0800

    A

---
+++ app/src/main/java/com/example/text/bitmapcompress/utils/ImageFactory.java
@@ -0,0 +1,180 @@
+package com.example.text.bitmapcompress.utils;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * Created by Chris on 2017/3/24.
+ */
+public class ImageFactory {
+
+    /**
+     * 从指定的路径加载
+     * 色值压缩（Bitmap.Config.RGB_565），其他不压缩
+     */
+    public Bitmap getBitmap(String imgPath) {
+        BitmapFactory.Options newOpts = new BitmapFactory.Options();
+        newOpts.inJustDecodeBounds = false;
+        newOpts.inPurgeable = true;
+        newOpts.inInputShareable = true;
+        newOpts.inSampleSize = 1;
+        newOpts.inPreferredConfig = Bitmap.Config.RGB_565;
+        return BitmapFactory.decodeFile(imgPath, newOpts);
+    }
+
+    /**
+     * 保存 Bitmap 到指定文件夹
+     */
+    public void storeImage(Bitmap bitmap, String outPath) throws FileNotFoundException {
+        FileOutputStream os = new FileOutputStream(outPath);
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, os);
+    }
+
+    /**
+     * newOpts.inSampleSize
+     * 大小压缩，生成缩略图
+     */
+    public Bitmap sizeCompress(String imgPath, float pixelW, float pixelH) {
+        BitmapFactory.Options newOpts = new BitmapFactory.Options();
+        // 开始读入图片，此时把options.inJustDecodeBounds 设回true，即只读边不读内容
+        newOpts.inJustDecodeBounds = true;
+        newOpts.inPreferredConfig = Bitmap.Config.RGB_565;
+        // Get bitmap info, but notice that bitmap is null now
+        Bitmap bitmap = BitmapFactory.decodeFile(imgPath, newOpts);
+
+        newOpts.inJustDecodeBounds = false;
+        int w = newOpts.outWidth;
+        int h = newOpts.outHeight;
+        // 想要缩放的目标尺寸
+        float hh = pixelH;// 设置高度为240f时，可以明显看到图片缩小了
+        float ww = pixelW;// 设置宽度为120f，可以明显看到图片缩小了
+        // 缩放比。由于是固定比例缩放，只用高或者宽其中一个数据进行计算即可
+        int be = 1;//be=1表示不缩放
+        if (w > h && w > ww) {//如果宽度大的话根据宽度固定大小缩放
+            be = (int) (newOpts.outWidth / ww);
+        } else if (w < h && h > hh) {//如果高度高的话根据宽度固定大小缩放
+            be = (int) (newOpts.outHeight / hh);
+        }
+        if (be <= 0) be = 1;
+        newOpts.inSampleSize = be;//设置缩放比例
+        // 开始压缩图片，注意此时已经把options.inJustDecodeBounds 设回false了
+        bitmap = BitmapFactory.decodeFile(imgPath, newOpts);
+        // 压缩好比例大小后再进行质量压缩
+//        return compress(bitmap, maxSize); // 这里再进行质量压缩的意义不大，反而耗资源，删除
+        return bitmap;
+    }
+
+    /**
+     * newOpts.inSampleSize
+     * 大小压缩，生成缩略图
+     */
+    public Bitmap sizeCompress(Bitmap image, float pixelW, float pixelH) {
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        image.compress(Bitmap.CompressFormat.JPEG, 100, os);
+        if (os.toByteArray().length / 1024 > 1024) {//判断如果图片大于1M,进行压缩避免在生成图片（BitmapFactory.decodeStream）时溢出
+            os.reset();//重置baos即清空baos
+            image.compress(Bitmap.CompressFormat.JPEG, 50, os);//这里压缩50%，把压缩后的数据存放到baos中
+        }
+        ByteArrayInputStream is = new ByteArrayInputStream(os.toByteArray());
+        BitmapFactory.Options newOpts = new BitmapFactory.Options();
+        //开始读入图片，此时把options.inJustDecodeBounds 设回true了
+        newOpts.inJustDecodeBounds = true;
+        newOpts.inPreferredConfig = Bitmap.Config.RGB_565;
+        Bitmap bitmap = BitmapFactory.decodeStream(is, null, newOpts);
+        newOpts.inJustDecodeBounds = false;
+        int w = newOpts.outWidth;
+        int h = newOpts.outHeight;
+        float hh = pixelH;// 设置高度为240f时，可以明显看到图片缩小了
+        float ww = pixelW;// 设置宽度为120f，可以明显看到图片缩小了
+        //缩放比。由于是固定比例缩放，只用高或者宽其中一个数据进行计算即可
+        int be = 1;//be=1表示不缩放
+        if (w > h && w > ww) {//如果宽度大的话根据宽度固定大小缩放
+            be = (int) (newOpts.outWidth / ww);
+        } else if (w < h && h > hh) {//如果高度高的话根据宽度固定大小缩放
+            be = (int) (newOpts.outHeight / hh);
+        }
+        if (be <= 0) be = 1;
+        newOpts.inSampleSize = be;//设置缩放比例
+        //重新读入图片，注意此时已经把options.inJustDecodeBounds 设回false了
+        is = new ByteArrayInputStream(os.toByteArray());
+        bitmap = BitmapFactory.decodeStream(is, null, newOpts);
+        //压缩好比例大小后再进行质量压缩
+//      return compress(bitmap, maxSize); // 这里再进行质量压缩的意义不大，反而耗资源，删除
+        return bitmap;
+    }
+
+    /**
+     * 质量压缩，不改变内存中大小，像素数
+     * maxSize 最大的大小
+     */
+    public void qualityCompressAndGenImage(Bitmap image, String outPath, int maxSize) throws IOException {
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        // scale
+        int options = 100;
+        // Store the bitmap into output stream(no compress)
+        image.compress(Bitmap.CompressFormat.JPEG, options, os);
+        // Compress by loop
+        while (os.toByteArray().length / 1024 > maxSize) {
+            // Clean up os
+            os.reset();
+            // interval 10
+            options -= 10;
+            image.compress(Bitmap.CompressFormat.JPEG, options, os);
+        }
+
+        // Generate compressed image file
+        FileOutputStream fos = new FileOutputStream(outPath);
+        fos.write(os.toByteArray());
+        fos.flush();
+        fos.close();
+    }
+
+    /**
+     * 质量压缩，不改变内存中大小，像素数
+     * maxSize 最大的大小
+     * needsDelete 是否需要删除
+     */
+    public void qualityCompressAndGenImage(String imgPath, String outPath, int maxSize, boolean needsDelete) throws IOException {
+        qualityCompressAndGenImage(getBitmap(imgPath), outPath, maxSize);
+
+        // Delete original file
+        if (needsDelete) {
+            File file = new File(imgPath);
+            if (file.exists()) {
+                file.delete();
+            }
+        }
+    }
+
+    /**
+     * 大小压缩，返回路径（即缩略图的路径）
+     */
+    public void sizeCompressAndGenThumb(Bitmap image, String outPath, float pixelW, float pixelH) throws FileNotFoundException {
+        Bitmap bitmap = sizeCompress(image, pixelW, pixelH);
+        storeImage(bitmap, outPath);
+    }
+
+    /**
+     * 大小压缩，返回路径（即缩略图的路径）
+     * needsDelete 是否删除
+     */
+    public void sizeCompressAndGenThumb(String imgPath, String outPath, float pixelW, float pixelH, boolean needsDelete) throws FileNotFoundException {
+        Bitmap bitmap = sizeCompress(imgPath, pixelW, pixelH);
+        storeImage(bitmap, outPath);
+
+        // Delete original file
+        if (needsDelete) {
+            File file = new File(imgPath);
+            if (file.exists()) {
+                file.delete();
+            }
+        }
+    }
+}
